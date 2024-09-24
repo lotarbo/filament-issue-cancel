80c0ec46fe02abe5af584475999c078677ec9ff3
Bogdan Lotar <bogdanlotar@gmail.com>
2024-09-24 18:56:00 +0200

    chore: init

---
+++ app/Filament/Resources/PageResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PageResource\Pages;
+use App\Filament\Resources\PageResource\RelationManagers;
+use App\Models\Page;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class PageResource extends Resource
+{
+    protected static ?string $model = Page::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Tabs::make('Tabs')
+                    ->persistTabInQueryString()
+                    ->tabs([
+                        Forms\Components\Tabs\Tab::make('General')->schema([
+                            Forms\Components\TextInput::make('title')
+                                ->required(),
+                            Forms\Components\TextInput::make('slug')
+                                ->required(),
+                        ]),
+
+                        Forms\Components\Tabs\Tab::make('SEO')->schema([
+                            Forms\Components\Section::make('seo')
+                                ->statePath('seo')
+                                ->schema([
+                                    Forms\Components\TextInput::make('title'),
+                                    Forms\Components\Textarea::make('description'),
+                                ])
+                        ]),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPages::route('/'),
+            'create' => Pages\CreatePage::route('/create'),
+            'edit' => Pages\EditPage::route('/{record}/edit'),
+        ];
+    }
+}
